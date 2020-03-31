7ef180ced20ceda3d5bb35c930539bbad50e2005
Anjalik19 <anjali.march19.com>
2020-03-31 18:58:19 +0530

    [Anjali]Add. Added InvoiceService.cs file

---
+++ CabInvoiceGenerator/InvoiceService .cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGenerator
+{
+    public class InvoiceService
+    {
+        int costPerMinute = 1;
+        double costPerKilometer = 10;
+        double minimumFare = 5;
+        
+        public double CalculateFare(double distance, double time)
+        {
+            double totalFare = distance * costPerKilometer + time * costPerMinute;
+            if(totalFare>minimumFare)
+            {
+                return totalFare;
+            }
+            return minimumFare;
+
+        }
+    }
+}
